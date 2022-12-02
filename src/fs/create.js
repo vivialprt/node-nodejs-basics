e1367fc3c2d568751d4d299003f25ad8fc53e15a
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-02 22:24:30 +0200

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import { writeFile } from 'fs/promises';
+
+
 const create = async () => {
-    // Write your code here 
+    try {
+        const content = 'I am fresh and young';
+        await writeFile('src/fs/files/fresh.txt', content, { flag: 'wx' });
+    } catch (err) {
+        console.log('FS operation failed')
+    }
+
 };
 
 await create();
