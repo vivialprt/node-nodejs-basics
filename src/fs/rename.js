5b7fa84bf8e1360f6a91bfb5cdf9b882be986b4e
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-03 18:21:26 +0200

    feat: rename file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import { rename as fsRename, access } from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const path = 'src/fs/files';
+    const incorrectName = 'wrongFilename.txt';
+    const correctName = 'properFilename.md';
+
+    let fullIncorrectName = [path, incorrectName].join('/');
+    let fullCorrectName = [path, correctName].join('/');
+
+    try {
+        await access(fullCorrectName);
+        console.log('here')
+        throw Error('FS operation failed');
+    } catch (err) {
+        if (err.code != 'ENOENT')
+            throw err;       
+    }
+
+    try {
+        await fsRename(fullIncorrectName, fullCorrectName);
+    } catch (err) {
+        if (err.code == 'ENOENT')
+            throw Error('FS operation failed');
+        else
+            throw err;
+    }
 };
 
 await rename();
