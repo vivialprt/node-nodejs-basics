e8efb351f15e309055d925fabe500525089a5439
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-03 21:43:20 +0200

    feat: create hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,10 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const fileName = 'src/hash/files/fileToCalculateHashFor.txt';
+    const buffer = await readFile(fileName);
+    console.log(createHash('sha256').update(buffer).digest('hex'));
 };
 
 await calculateHash();
