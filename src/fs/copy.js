a456a73274e6bade0aca0658b595baa91df4f7b1
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-03 16:30:38 +0200

    feat: copy dir

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { cp, mkdir } from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+    const src = 'src/fs/files';
+    const dst = 'src/fs/files_copy';
+    try {
+        await mkdir(dst);
+        await cp(src, dst, { recursive: true });
+    } catch (err) {
+        if (err.code == 'EEXIST' || err.code == 'ENOENT')
+            throw Error('FS operation failed');
+        else
+            throw err;
+    }
 };
 
 copy();
