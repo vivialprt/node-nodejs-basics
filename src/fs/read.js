3128c53044e2db5a9a4b96664ef8c9874ae47129
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-03 18:42:42 +0200

    feat: read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = 'src/fs/files/fileToRead.txt';
+
+    try {
+        let fileContents = await readFile(filePath, { encoding: 'utf8' });
+        console.log(fileContents);
+    } catch (err) {
+        if (err.code == 'ENOENT')
+            throw Error('FS operation failed');
+        else
+            throw err;
+    }
 };
 
 await read();
