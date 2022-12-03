1f452bb7f13daba42df5e49d436ee7c5bfca10d6
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-03 19:30:28 +0200

    feat: parse args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = [];
+
+    for (let i = 0; i < process.argv.length; i++)
+        if (process.argv[i].startsWith('--'))
+            args.push(`${process.argv[i].replace('--', '')} is ${process.argv[i + 1]}`);
+
+    console.log(args.join(', '));
 };
 
 parseArgs();
