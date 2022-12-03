8231bfe15036d020d399d18434754aaee0a9b10c
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-03 18:36:20 +0200

    feat: list dir

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { readdir } from 'fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    const dirPath = 'src/fs/files';
+
+    try {
+        let dirContents = await readdir(dirPath);
+        for(let fName of dirContents)
+            console.log(fName);
+    } catch (err) {
+        if (err.code == 'ENOENT')
+            throw Error('FS operation failed');
+        else
+            throw err;
+    }
 };
 
 await list();
