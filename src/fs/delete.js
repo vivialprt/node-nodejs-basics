8801b709f08601d7f011221f18c0cfa28b4cae59
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-03 18:30:00 +0200

    feat: delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { rm } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = 'src/fs/files/fileToRemove.txt';
+
+    try {
+        await rm(filePath);
+    } catch (err) {
+        if (err.code == 'ENOENT')
+            throw Error('FS operation failed');
+        else
+            throw err;
+    }
 };
 
 await remove();
