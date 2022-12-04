cb30e3c097f729efbc7c2c9ff356afa402ff00d1
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-04 23:05:33 +0200

    feat: worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,15 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerScript = './src/wt/worker.js';
+    const workers = [];
+    for(let i = 0; i < cpus().length; i++) {
+        const worker = new Worker(workerScript, { workerData: 10 + i});
+        worker.on('message', (msg) => {console.log(msg)})
+        workers.push(worker);
+        
+    }
 };
 
 await performCalculations();
