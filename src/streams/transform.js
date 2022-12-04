024e4c4225694dbf9ddc898cd155ac0f22071b81
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-04 21:25:13 +0200

    feat: transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+    const stream = new Transform({
+        transform (data, encoding, callback) {
+            const reversedData = reverseString(data);
+            this.push(reversedData);
+            callback();
+        }
+    });
+    process.stdin.pipe(stream).pipe(process.stdout);
 };
+
+const reverseString = (data) => {
+    return data.toString().split('').reverse().join('');
+}
 
 await transform();
