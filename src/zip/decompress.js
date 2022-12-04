e7735eb426a64b603815d8c083491d255243438d
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-04 21:55:58 +0200

    feat: decompress archive

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import { createGunzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { promisify } from 'node:util';
+
 const decompress = async () => {
-    // Write your code here 
+    const src = 'src/zip/files/archive.gz';
+    const dst = 'src/zip/files/fileToCompress2.txt';
+    const pipe = promisify(pipeline);
+
+    try {
+
+        const readStream = createReadStream(src);
+        const writeStream = createWriteStream(dst);
+        const decompressStream = createGunzip();
+        await pipe(readStream, decompressStream, writeStream);
+
+    } catch (err) {
+        if (err.code == 'ENOENT')
+            throw Error('No such file');
+        else
+            throw Error('Pishi ashibku');
+    }
+
 };
 
 await decompress();
