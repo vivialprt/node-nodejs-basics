6e2dfb1a840e9d34c5f6a4f50bcf1592978d8460
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-04 21:47:53 +0200

    feat: compress file

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { promisify } from 'node:util';
+
 const compress = async () => {
-    // Write your code here 
+    const src = 'src/zip/files/fileToCompress.txt';
+    const dst = 'src/zip/files/archive.gz';
+    const pipe = promisify(pipeline);
+
+    try {
+
+        const readStream = createReadStream(src);
+        const writeStream = createWriteStream(dst);
+        const compressStream = createGzip();
+        await pipe(readStream, compressStream, writeStream);
+
+    } catch (err) {
+        if (err.code == 'ENOENT')
+            throw Error('No such file');
+        else
+            throw Error('Pishi ashibku');
+    }
+
 };
 
 await compress();
