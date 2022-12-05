163bc357b500118fa1ba06c031fa570980c7fb20
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-05 22:47:51 +0200

    fix: correct results

--- src/wt/main.js
+++ src/wt/main.js
@@ -3,13 +3,26 @@ import { cpus } from 'node:os';
 
 const performCalculations = async () => {
     const workerScript = './src/wt/worker.js';
-    const workers = [];
+    const workerPromises = [];
     for(let i = 0; i < cpus().length; i++) {
-        const worker = new Worker(workerScript, { workerData: 10 + i});
-        worker.on('message', (msg) => {console.log(msg)})
-        workers.push(worker);
-        
+        const workerPromise = new Promise(resolve => {
+            try {
+                const worker = new Worker(workerScript, { workerData: 10 + i});
+                worker.on('message', (msg) => {
+                    resolve({status: 'resolved', data: msg});
+                });
+                worker.on('error', (err) => {
+                    resolve({status: 'error', data: err});
+                });
+            } catch (err) {
+                resolve({status: 'promise error', data: null});  //prevent fast fail for Promise.all()
+            }
+        });
+        workerPromises.push(workerPromise);
     }
+    const workerResults = await Promise.all(workerPromises);
+    console.log(workerResults);
+
 };
 
 await performCalculations();
