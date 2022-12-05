9f5073de20b90c510f156343e6ca8095c8fbe914
Ivan D. Grigoryev <ivan.grigoryev502@gmail.com>
2022-12-05 23:17:45 +0200

    feat: child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,18 @@
+import { execFile } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const fName = './src/cp/files/script.js';
+    args.unshift(fName);
+    const child = execFile('node', args, (error, stdout, stderr) => {
+        if (error) {
+            throw error;
+        }
+        console.log(stdout);
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
 };
 
 spawnChildProcess();
